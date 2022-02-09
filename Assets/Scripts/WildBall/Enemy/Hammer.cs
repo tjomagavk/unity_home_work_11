c79714cbabf2af3bf5f8091ee908f1e94b4396c6
tjomagavk <tjomagavk@yandex.ru>
2022-02-09 23:16:40 +0300

    Добавлено отталкивание молота

---
+++ Assets/Scripts/WildBall/Enemy/Hammer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using WildBall.Constants;
+
+namespace WildBall.Enemy
+{
+    public class Hammer : MonoBehaviour
+    {
+        [SerializeField] private HammerWorkArea hammerWorkArea;
+
+        public void StartPosition()
+        {
+            hammerWorkArea.StartPosition();
+        }
+    }
+}
---
+++ Assets/Scripts/WildBall/Enemy/HammerWorkArea.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using WildBall.Constants;
+
+namespace WildBall.Enemy
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class HammerWorkArea : MonoBehaviour
+    {
+        private Vector3 startPosition;
+
+        public void StartPosition()
+        {
+            startPosition = gameObject.transform.position;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(TagVars.Player))
+            {
+                Vector3 vector3 = startPosition - gameObject.transform.position;
+                vector3.x = -vector3.x;
+                vector3.z = -vector3.z;
+                other.GetComponent<Rigidbody>().AddForce(vector3.normalized * 20, ForceMode.Impulse);
+            }
+        }
+    }
+}
