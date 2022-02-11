3eb36e08d05c92f6322ef74cbb22371a9aa01931
tjomagavk <tjomagavk@yandex.ru>
2022-02-11 09:15:15 +0300

    Добавлено перемещение лифта и взрыв

---
+++ Assets/Scripts/WildBall/Enemy/Bomb.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using WildBall.Constants;
+
+namespace WildBall.Enemy
+{
+    public class Bomb : MonoBehaviour
+    {
+        [SerializeField] private int boomRadius;
+        [SerializeField] private float boomPower;
+        private Rigidbody[] physicObjects;
+
+        private void Start()
+        {
+            physicObjects = FindObjectsOfType(typeof(Rigidbody)) as Rigidbody[];
+        }
+
+        public void Boom()
+        {
+            foreach (Rigidbody physicObject in physicObjects)
+            {
+                var distance = Vector3.Distance(gameObject.transform.position, physicObject.transform.position);
+                if (distance < boomRadius)
+                {
+                    physicObject.GetComponent<Rigidbody>()
+                        .AddExplosionForce(boomPower, gameObject.transform.position, boomRadius);
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/WildBall/Mechanism/ElevatorController.cs
+++ Assets/Scripts/WildBall/Mechanism/ElevatorController.cs
@@ -1,16 +1,56 @@
-﻿using UnityEngine;
-using WildBall.Constants;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace WildBall.Mechanism
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class ElevatorController : MonoBehaviour
     {
-        [SerializeField] private Animator animator;
+        [SerializeField] private Transform pointDown;
+        [SerializeField] private Transform pointUp;
         [SerializeField] private LeverHandleController leverOne;
         [SerializeField] private LeverHandleController leverTwo;
 
+        private Rigidbody elevatorRigidbody;
+        public float timeSec = 5f;
+
         private bool leverOneUpPosition = true;
         private bool leverTwoUpPosition = true;
+
+        private void Awake()
+        {
+            elevatorRigidbody = GetComponent<Rigidbody>();
+            StartCoroutine(MoveObject(pointDown.position, pointUp.position, timeSec));
+        }
+
+        public void Move()
+        {
+            Debug.Log("Pos: " + transform + pointDown + pointUp);
+            if (transform.position == pointDown.position)
+            {
+                StartCoroutine(MoveObject(pointDown.position, pointUp.position, timeSec));
+            }
+            else if (transform.position == pointUp.position)
+            {
+                StartCoroutine(MoveObject(pointUp.position, pointDown.position, timeSec));
+            }
+        }
+
+
+        private IEnumerator MoveObject(Vector3 from, Vector3 to, float timeSec)
+        {
+            float currentTime = 0f;
+            elevatorRigidbody.MovePosition(from);
+
+            while (currentTime <= timeSec)
+            {
+                currentTime += Time.fixedDeltaTime;
+                yield return new WaitForFixedUpdate();
+
+                Vector3 newPosition = Vector3.Lerp(from, to, currentTime / timeSec);
+                elevatorRigidbody.MovePosition(newPosition);
+            }
+        }
 
         private void LateUpdate()
         {
@@ -19,20 +59,15 @@ namespace WildBall.Mechanism
                 leverOneUpPosition = leverOne.UpPosition;
                 leverTwoUpPosition = leverOneUpPosition;
                 leverTwo.SetPosition(leverOneUpPosition);
-                ChangePosition();
+                Move();
             }
             else if (leverTwo.UpPosition != leverTwoUpPosition)
             {
                 leverTwoUpPosition = leverTwo.UpPosition;
                 leverOneUpPosition = leverTwoUpPosition;
                 leverOne.SetPosition(leverTwoUpPosition);
-                ChangePosition();
+                Move();
             }
-        }
-
-        private void ChangePosition()
-        {
-            animator.SetTrigger("ChangePosition");
         }
     }
 }
